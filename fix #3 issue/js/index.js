bd524333a08ff26f3ae03285d98fdae1d6e10df2
Mo'men Soliman <moumensoliman@gmail.com>
2019-03-28 22:25:01 +0200

    Fix push array to another
    Fix push array to another issue
    
    Fix #3

---
+++ fix #3 issue/js/index.js
@@ -0,0 +1,8 @@
+//Create Fruits Array
+const fruits = ['banana', 'apple'];
+//Create Sauces Array
+const sauces = ['honey', 'sugar'];
+//Merge Two Arrays
+const fruitsSauces = [...fruits, ...sauces];
+console.logt(fruitsSauces);
+
