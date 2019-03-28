57860493cf14e48a7fb9145c1d30f8e6962ef84e
Mo'men Soliman <moumensoliman@gmail.com>
2019-03-28 22:23:25 +0200

    Fix menu issue
    Fix menu issue by merge 3 arrays into one
    
    Fix #1

---
+++ fix #1 issue/js/index.js
@@ -0,0 +1,9 @@
+//Create Pizza Array
+const pizza = ['peproni', 'margherita'];
+//Create Appetizers Array
+const appetizers = ['bread', 'salad'];
+//Create Sauces Array
+const sauces = ['bbq', 'ranch'];
+//Create Menu Array
+const menu = [...pizza, ...appetizers, ...sauces];
+console.log(menu);
